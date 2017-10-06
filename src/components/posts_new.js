8e3c0513e360b185d9a9c2c912da51de8087ddf8
Maksim Korzhov <isitbux@gmail.com>
2017-10-06 22:35:00 +0300

    Added form validation.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,28 +3,40 @@ import {Field, reduxForm} from "redux-form";
 
 class PostsNew extends Component {
     renderField(field) {
+        const {meta: {touched, error}} = field;
+        const className = `form-group ${touched && error ? "has-danger" : ""}`;
+
         return(
-            <div className="form-group">
+            <div className={className   }>
                 <label htmlFor="">{field.label}</label>
                 <input
                     className="form-control"
                     {...field.input}
                 />
+                <div className="text-danger">{touched ? error : ""}</div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        // this === component
+        console.log(values);
+    }
+
     render() {
+        const {handleSubmit} = this.props;
+
         return (
-            <form>
+            // Если с формой всё нормально, то вызовем onSubmit
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title"
                     name="title"
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -32,11 +44,38 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    // Validate inputs
+    if(values.title && values.title.length < 3) {
+        errors.title = "Enter a title that is at least 3 characters!";
+    }
+
+    if(!values.title) {
+        errors.title = "Enter a title!";
+    }
+
+    if(!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+
+    if(!values.content) {
+        errors.content = "Enter some content";
+    }
+
+    // If errors is empty, the form is fine to submit
+    // If errors has *any* properties, redux form assumes form is invalid
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: "PostsNewForm" // Name of the form. Must be unique!
 })(PostsNew);
